33904eddcb57a173f63f50393d28d97c2afbd286
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2025-04-07 13:49:50 +0200

    optimsation flags, not sure they work for c++

--- codingGame.cpp
+++ codingGame.cpp
@@ -1,3 +1,11 @@
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
 #include <iostream>
 #include <string>
 #include <vector>
